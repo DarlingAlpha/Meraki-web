c3489ccac0384e67574728352c999137877aa80b
DarlingAlpha <ggl4@discente.ifpe.edu.br>
2023-11-22 12:18:33 -0300

    Lista de serviçõs feita falta Cadastro

--- src/views/servicos/CadastroServicos.jsx
+++ src/views/servicos/CadastroServicos.jsx
@@ -1,52 +1,94 @@
 import React, { useState } from 'react';
-import { Header, Icon, Image, Label, Table } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Button, Header, Image, Rating, Table } from 'semantic-ui-react';
 
-export default function PerfilCliente() {
-  const [lista] = useState([
+export default function ServicosdoFornecedor() {
+  const [Fornecedor] = useState([
     {
       id:"01",
-      nome:"Gabriel",
-      cpf:"1321321312",
-      foneCelular:"23213213213",
-      foneFixo:"2132321312"
+      Foto:"https://as2.ftcdn.net/v2/jpg/05/86/91/55/1000_F_586915596_gPqgxPdgdJ4OXjv6GCcDWNxTjKDWZ3JD.jpg",
+      NomeFornecedor:"Gabriel",
+      
     }
+
+  ])
+  const [Servico] = useState([
+    {
+      id:"01",
+      NomeFornecedor:"Gabriel",
+      Categoria:"Buffet",
+      decoracoes:"Infantil",
+      Avaliações:"defaultRating={0} maxRating={5}",
+      Regiao:"Recife",
+      Detalhes:"Buffe decorativo infantil"
+    },{
+      id:"01",
+      Categoria:"Decoração",
+      decoracoes:"Infantil",
+      Avaliações:"defaultRating={0} maxRating={5}",
+      Regiao:"Jaboatão dos Guararapes",
+      Detalhes:"Decoração decorativo infantil"
+    }
+
   ]);
    
     return (
+
+  
   <Header as='h2' >
-    <Image circular src='https://as2.ftcdn.net/v2/jpg/05/86/91/55/1000_F_586915596_gPqgxPdgdJ4OXjv6GCcDWNxTjKDWZ3JD.jpg' />
-    Metodo Get Nome do Cliente
-    <div>
-    <Label>
-    <Icon name='mail' /> 23
-    </Label>
+    {Fornecedor.map(Fornecedor => (
+    <div key={Fornecedor.id} style={{display:'flex', alignItems:'center'}}>
+      {/* Falta Imagem */}
+    <Image circular 
+    src={Fornecedor.Foto} style={{width:'50px', height:'50px'}}>
+    
+    </Image>
+    {/* Nome do fornecedor */}
+    <div >
+    {Fornecedor.NomeFornecedor}
     </div>
+    </div>
+     ))}
+  
 
     <body>
-  <Table color='yellow' sortable celled>
-  <Table.Header >
-          <Table.Row>
-            <Table.HeaderCell>Nome</Table.HeaderCell>
-            <Table.HeaderCell>CPF</Table.HeaderCell>
-            <Table.HeaderCell>Fone Celular</Table.HeaderCell>
-            <Table.HeaderCell>Fone Fixo</Table.HeaderCell>
-            <Table.HeaderCell textAlign='center'>Editar</Table.HeaderCell>
-        </Table.Row>
+      <div>
+        <Button ><Link to={'/PerfilFornecedor'}>Novo Serviço</Link></Button>
+      </div>
+    <Table celled padded>
+    <Table.Header>
+      <Table.Row>
+<Table.HeaderCell singleLine>Categoria</Table.HeaderCell>
+        <Table.HeaderCell>decorações</Table.HeaderCell>
+        <Table.HeaderCell>Avaliações</Table.HeaderCell>
+        <Table.HeaderCell>Região</Table.HeaderCell>
+        <Table.HeaderCell>Detalhes</Table.HeaderCell>
+      </Table.Row>
     </Table.Header>
-    {lista.map(cliente => (
 
-<Table.Row key={cliente.id}>
-    <Table.Cell>{cliente.nome}</Table.Cell>
-    <Table.Cell>{cliente.cpf}</Table.Cell>
-    <Table.Cell>{cliente.foneCelular}</Table.Cell>
-    <Table.Cell>{cliente.foneFixo}</Table.Cell>
-    <Table.Cell textAlign='center'>
-
-    </Table.Cell>
-</Table.Row>
-))}
-
+    <Table.Body>
+    {Servico.map(Servico => (
+      <Table.Row key={Servico.id}>
+        <Table.Cell>
+          <Header as='h3' textAlign='center'>
+            {Servico.Categoria}
+          </Header>
+        </Table.Cell>
+        <Table.Cell singleLine>{Servico.decoracoes}</Table.Cell>
+        <Table.Cell>
+          <Rating icon='star' defaultRating={0} maxRating={5} disabled/>
+        </Table.Cell>
+        <Table.Cell textAlign='right'>
+        {Servico.Regiao}
+        </Table.Cell>
+        <Table.Cell>
+        {Servico.Detalhes}
+        </Table.Cell>
+      </Table.Row>
+    ))}
+    </Table.Body>
   </Table>
+ 
 </body>
   
     
